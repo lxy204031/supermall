0aabaf9c26de476a2b2326700b7cbbd504305191
BreadLee <1206606250@qq.com>
2022-07-18 16:51:49 +0800

    测试vscode

--- src/components/common/navbar/NavBar.vue
+++ src/components/common/navbar/NavBar.vue
@@ -15,7 +15,6 @@ export default {
 <style scoped>
   .nav-bar {
     display: flex;
-    /*height: 44px;*/
     line-height: 44px;
     text-align: center;
     box-shadow: 0 1px 10px rgba(100, 100, 100, .4);
